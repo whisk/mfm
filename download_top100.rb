a48f07766b421bb666f69851a9d2edc53d45a998
Alex Svetkin <alex@svetkin.ru>
2012-02-27 00:17:05 +0400

    now works with cmdline params

--- download_top100.rb
+++ download_top100.rb
@@ -2,18 +2,32 @@
 
 require 'xmlsimple'
 require 'lib/mfm'
+require 'open-uri'
 
-data = XmlSimple.xml_in File.open(ARGV[0]), {'ForceArray' => false}
+station = ARGV[0]
+basedir = ARGV[1] || '.'
+
+station_playlist_url = "http://www.moskva.fm/player_xml.playlist.html?playlist=null&__xmlchart=stations&__xmltype=#{URI.escape(station)}&__xmladd=&rnd=0%2E36734794871881604"
+
+print "Downloading station '#{station}' playlist...\n"
+data = XmlSimple.xml_in open(station_playlist_url).readlines.join(''), {'ForceArray' => false}
+print "Done\n"
+
+Dir.mkdir(basedir) unless File.directory?(basedir)
 
 MFM.init(YAML.load_file('settings.yml'))
 i = 1
 data['tracks']['track'].each do |t|
   track = MFM::Track.new(t)
-  mp3_file = "#{track.artistname} - #{track.trackname}.mp3"
+  mp3_file = "#{basedir}/#{track.artistname} - #{track.trackname}.mp3"
   print "Saving track ##{i} into '#{mp3_file}'... "
+
   unless File.exist?(mp3_file)
     begin
       track.save_as_mp3(mp3_file)
+    rescue Interrupt => e
+      puts "Interrupting..."
+      break
     rescue Exception => e
       puts e.message
     end
