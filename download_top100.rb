a35de278654622bde3ca32a926c6429660791b48
Alex Svetkin <alex@svetkin.ru>
2012-02-26 23:38:13 +0400

    exception handling

--- download_top100.rb
+++ download_top100.rb
@@ -6,7 +6,21 @@ require 'lib/mfm'
 data = XmlSimple.xml_in File.open(ARGV[0]), {'ForceArray' => false}
 
 MFM.init(YAML.load_file('settings.yml'))
+i = 1
 data['tracks']['track'].each do |t|
   track = MFM::Track.new(t)
-  track.save_as_mp3("#{track.artistname} - #{track.trackname}.mp3")
+  mp3_file = "#{track.artistname} - #{track.trackname}.mp3"
+  print "Saving track ##{i} into '#{mp3_file}'... "
+  unless File.exist?(mp3_file)
+    begin
+      track.save_as_mp3(mp3_file)
+    rescue Exception => e
+      puts e.message
+    end
+  else
+    print "Skipping, file already exists."
+  end
+
+  print "\n"
+  i += 1
 end
